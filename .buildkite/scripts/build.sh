584eaebb5befbc4cb03d5228cf4f8318bbe37ab9
Michael Wolf <michael.wolf@elastic.co>
2024-10-04 16:29:28 -0700

    Build arm64 artifacts in CI (#5)
    Set up the buildkite pipeline and scripts to build arm64 artifacts in an arm64 VM. This will also reduce some unnecessary building and committing by automation, by skipping all builds if the previous commit was created by automation, and not trying to push artifacts directly to main (which is rejected by repo rules anyway).

--- .buildkite/scripts/build.sh
+++ .buildkite/scripts/build.sh
@@ -2,12 +2,18 @@
 
 set -euo pipefail
 
-# Build libquark_big.a for all supported architectures
+# set expected arch name
+ARCH="$(uname -m)"
+if [ "${ARCH}" = "x86_64" ]; then
+	ARCH=amd64
+elif [ "${ARCH}" = "aarch64" ]; then
+	ARCH=arm64
+fi
+
+echo "Building on ${ARCH}"
 
 git submodule update --init --recursive
 
-for ARCH in amd64
-do
-	ARCH=$ARCH make -C src centos7
-	mv src/libquark_big.a libquark_big_${ARCH}.a
-done
+make -C src centos7
+
+mv src/libquark_big.a libquark_big_${ARCH}.a
